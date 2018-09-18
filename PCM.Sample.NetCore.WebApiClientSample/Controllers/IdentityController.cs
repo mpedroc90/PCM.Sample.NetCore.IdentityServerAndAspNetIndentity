2e1fb9a71e24ad9829fc79f94a3bd3bfa34c3f4a
mpedroc90 <mpedroc90@gmail.com>
2018-09-18 17:46:31 -0400

    Sample of Identity

---
+++ PCM.Sample.NetCore.WebApiClientSample/Controllers/IdentityController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PCM.Sample.NetCore.WebApiClientSample.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController: Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
+       
+    }
+}
